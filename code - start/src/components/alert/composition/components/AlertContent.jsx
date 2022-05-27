8b1a47cdf3264e4c20e48431db4872b34603547f
Thomas <thomasfindlay94@gmail.com>
2022-05-27 15:55:39 +0100

    Removed unnecessary dir

--- code - start/src/components/alert/composition/components/AlertContent.jsx
+++
@@ -1,9 +0,0 @@
-import clsx from "clsx";
-import styles from "../../alert.module.scss";
-
-const AlertContent = props => {
-  const { className, children } = props;
-  return <div className={clsx(styles.alertContent, className)}>{children}</div>;
-};
-
-export default AlertContent;
--- code - start/src/components/alert/composition/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AlertIcon } from "./AlertIcon";
-export { default as AlertHeader } from "./AlertHeader";
-export { default as AlertBody } from "./AlertBody";
-export { default as AlertContent } from "./AlertContent";
